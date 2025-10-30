7ac7871cc7470dd71c9932674b39b85dbccf17fe
whitecomunicacoes-cloud <whitecomunicacoes@gmail.com>
2025-10-30 11:45:07 -0300

    Add files via upload

---
+++ vercel.js
@@ -0,0 +1,24 @@
+{
+  "redirects": [
+    {
+      "source": "/tiktok",
+      "destination": "https://caixadapromessa.com.br/?utm_source=tiktok&utm_medium=short&utm_campaign=promessa_diaria",
+      "permanent": true
+    },
+    {
+      "source": "/facebook",
+      "destination": "https://caixadapromessa.com.br/?utm_source=facebook&utm_medium=reel&utm_campaign=promessa_diaria",
+      "permanent": true
+    },
+    {
+      "source": "/youtube",
+      "destination": "https://caixadapromessa.com.br/?utm_source=youtube&utm_medium=short&utm_campaign=promessa_diaria",
+      "permanent": true
+    },
+    {
+      "source": "/whatsapp",
+      "destination": "https://caixadapromessa.com.br/?utm_source=whatsapp&utm_medium=mensagem&utm_campaign=promessa_diaria",
+      "permanent": true
+    }
+  ]
+}
